9ebcf56406e53c6e63a943fea1ae2e6cde61c05c
B.Sravya <119840452+22A91A61E2@users.noreply.github.com>
2022-12-12 11:09:51 +0530

    #include<stdio.h> int main() {     int n=3,i,c,p=1;      int a[n];      for(i=0;i<n;i++)       {          scanf("%d",&a[i]);       }      for(i=0;i<n;i++)       {          p=p*a[i];          c=(2*p*512)/1024;       }          printf("%dKB",c);
    }

--- Find_the_Capacity.c
+++ Find_the_Capacity.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n=3,c,i,p=1;
-    int a[3];
-    for(i=0;i<3;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<3;i++)
-    {
-        p=p*a[i];
-        c=(2*p*512)/1024;
-    }    
-    printf("%dKB",c);
+    int n=3,i,c,p=1;
+     int a[n];
+     for(i=0;i<n;i++)
+      {
+         scanf("%d",&a[i]);
+      }
+     for(i=0;i<n;i++)
+      {
+         p=p*a[i];
+         c=(2*p*512)/1024;
+      }
+         printf("%dKB",c);
+  
 }
