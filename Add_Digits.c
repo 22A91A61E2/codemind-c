e3e89d3e3860ceabd6cd77fdfcff3b6309e09d4b
B.Sravya <119840452+22A91A61E2@users.noreply.github.com>
2023-05-21 08:47:11 +0530

    #include <stdio.h> int addDigits(int number) {     while (number > 9) {         int sum = 0;         while (number > 0) {             sum += number % 10;             number /= 10;         }         number = sum;     }     return number; } int main() {     int number;     scanf("%d", &number);     int result = addDigits(number);     printf("%d",result);     return 0; }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,17 +1,19 @@
-#include<stdio.h>
-int main()
-{
-    int n,d,sum=0;
-    scanf("%d",&n);
-    while(n>0||sum>9)
-    {
-        if(n==0)
-        {
-            n=sum;
-            sum=0;
+#include <stdio.h>
+int addDigits(int number) {
+    while (number > 9) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
         }
-        sum=sum+n%10;
-        n=n/10;
+        number = sum;
     }
-    printf("%d",sum);
+    return number;
+}
+int main() {
+    int number;
+    scanf("%d", &number);
+    int result = addDigits(number);
+    printf("%d",result);
+    return 0;
 }
